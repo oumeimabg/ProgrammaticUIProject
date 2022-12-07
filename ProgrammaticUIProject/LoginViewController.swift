d209f8079d0438422ca960c4b6b8df60a2f673e2
Oumeimabg <oumayma.ben-ghalba@proxym-it.com>
2022-12-07 14:33:46 +0100

    Launch app with custom ViewController

---
+++ ProgrammaticUIProject/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  ProgrammaticUIProject
+//
+//  Created by Oumeima.ben-Ghalba on 7/12/2022.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
