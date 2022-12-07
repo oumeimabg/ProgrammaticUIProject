ab04e57ff21505acf26646c4d29c528f1309006b
Oumeimabg <oumayma.ben-ghalba@proxym-it.com>
2022-12-07 13:58:42 +0100

    Initial Commit

---
+++ ProgrammaticUIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIProject
+//
+//  Created by Oumeima.ben-Ghalba on 7/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
